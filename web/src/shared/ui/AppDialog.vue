04fff37987f6fb59e971be8c456140f4ce56e59c
nerdinhisprime <nerdinhisprime@gmail.com>
2026-07-13 08:58:22 +0000

    fix missed target

--- web/src/shared/ui/AppDialog.vue
+++ web/src/shared/ui/AppDialog.vue
@@ -1,15 +1,28 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
+  import { ref } from 'vue';
 
-  const dialogRef = ref<HTMLDialogElement | null>(null)
+  const dialogRef = ref<HTMLDialogElement | null>(null);
+
+  const removeFocus = () => {
+    if (document.activeElement instanceof HTMLElement) {
+      document.activeElement.blur();
+    };
+  };
+
   defineExpose({
     open: () => dialogRef.value?.showModal(),
     close: () => dialogRef.value?.close(),
-  })
+  });
 </script>
 
 <template>
-  <dialog ref="dialogRef" @click="dialogRef?.close()">
+  <dialog
+    ref="dialogRef"
+    @click="
+      dialogRef?.close();
+      removeFocus();
+    "
+  >
     <div @click.stop>
       <slot />
     </div>
